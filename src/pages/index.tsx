8832160e654568b27d3c837083f69f04ef4e61ea
Maky <franklinjaradev@gmail.com>
2022-04-12 19:22:53 -0400

    Adding tailwindcss and tsconfig

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+  <div className="bg-blue-100">
+      <h1 className="text-center">HELLO</h1>
+    </div>
+  )
+}
+
+export default Home
