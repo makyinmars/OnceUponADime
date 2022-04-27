f744212361398310c7002a9091ea921036299719
Maky <franklinjaradev@gmail.com>
2022-04-26 22:03:19 -0400

    chore: Adding links for navbar

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,10 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="flex">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
+    <footer className="flex justify-center">
+      Copyright Once Upon A Dime 2022
     </footer>
   );
 };
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,11 +1,18 @@
 import { FcBinoculars } from "react-icons/fc";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex justify-around font-header bg-violet-400 py-6">
-      <div className="font-title">Once Upon A Dime</div>
-      <div>Blog</div>
-      <div>Contact</div>
+      <div className="font-title">
+        <Link href="/">Once Upon A Dime</Link>
+      </div>
+      <div>
+        <Link href="/blog">Blog</Link>
+      </div>
+      <div>
+        <Link href="/contact">Contact</Link>
+      </div>
       <div className="flex flex-column justify-center items-center gap-2">
         Search <FcBinoculars />
       </div>
