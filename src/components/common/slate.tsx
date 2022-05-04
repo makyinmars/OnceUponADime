24a78aa8924d0f29d4873678ad5b64bb0da2bccb
Franklin Jara <franklinjaradev@gmail.com>
2022-05-03 21:29:45 -0400

    task: working on slate

--- src/components/common/slate.tsx
+++ src/components/common/slate.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from "react"
-import { BaseEditor, createEditor, Descendant } from "slate"
+import React, { useCallback, useMemo } from "react"
+import {
+  BaseEditor,
+  createEditor,
+  Descendant,
+  Editor,
+  Transforms,
+  Node,
+} from "slate"
 import { ReactEditor, withReact, Slate, Editable } from "slate-react"
-
 interface CustomText {
   text: string
 }
 interface CustomElement {
-  type: "paragraph"
+  type: "paragraph" | "code"
   children: CustomText[]
 }
 
@@ -18,15 +24,85 @@ declare module "slate" {
   }
 }
 
-const initialValue: Descendant[] = [
-  { type: "paragraph", children: [{ text: "A line of text in a paragraph." }] },
-]
+// Define a serializing function that takes a value and returns a string.
+const serialize = (value: any) => {
+  return (
+    value
+      // Return the string content of each paragraph in the value's children.
+      .map((n) => Node.string(n))
+      // Join them all with line breaks denoting paragraphs.
+      .join("\n")
+  )
+}
+
+// Define a deserializing function that takes a string and returns a value.
+const deserialize = (value: any) => {
+  // Return a value array of children derived by splitting the string.
+  return value.split("\n").map((line: any) => {
+    return {
+      children: [{ text: line }],
+    }
+  })
+}
+
+const CodeElement = (props: any) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+const DefaultElement = (props: any) => {
+  return <p {...props.attributes}>{props.children}</p>
+}
 
 const SlateBlog = () => {
-  const [editor] = useState(() => withReact(createEditor()))
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const initialValue: any = useMemo(
+    deserialize(localStorage.getItem("content")) || "",
+    []
+  )
+
+  const renderElement = useCallback((props: any) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
   return (
-    <Slate editor={editor} value={initialValue}>
-      <Editable />
+    <Slate
+      editor={editor}
+      value={initialValue}
+      onChange={(value) => {
+        const isAstChange = editor.operations.some(
+          (op) => "set_selection" !== op.type
+        )
+        if (isAstChange) {
+          // Save the value to Local Storage.
+          const content = JSON.stringify(value)
+          localStorage.setItem("content", content)
+        }
+      }}
+    >
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={(event) => {
+          if (event.key === "`" && event.ctrlKey) {
+            event.preventDefault()
+            const [match] = Editor.nodes(editor, {
+              match: (n) => n.type === "code",
+            })
+            Transforms.setNodes(
+              editor,
+              { type: match ? "paragraph" : "code" },
+              { match: (n) => Editor.isBlock(editor, n) }
+            )
+          }
+        }}
+      />
     </Slate>
   )
 }
