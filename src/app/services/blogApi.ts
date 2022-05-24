ae5c48b90164d7a6e1dc45f202ea5ee3c353bb69
Franklin Jara <franklinjaradev@gmail.com>
2022-05-24 18:30:38 -0400

    task: adding blogApi for global state

---
+++ src/app/services/blogApi.ts
@@ -0,0 +1,57 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+import { Blog } from "@/types/blog"
+import { API_URL } from "@/constants"
+import { RootState } from "../store"
+
+export const blogApi = createApi({
+  reducerPath: "blogApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${API_URL}/api/blog`,
+    // By default, if we have a token in the store, let's use that for authenticated requests
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`)
+      }
+      return headers
+    },
+  }),
+  tagTypes: ["Blog"],
+  endpoints: (builder) => ({
+    getBlogs: builder.query<Blog[], void>({
+      query: () => ({
+        url: "/",
+        method: "GET",
+      }),
+      providesTags: ["Blog"],
+    }),
+    getBlog: builder.query<Blog, string>({
+      query: (_id) => `/${_id}`,
+      providesTags: ["Blog"],
+    }),
+    updateBlog: builder.mutation<Blog, Blog>({
+      query: ({ _id, author, content, draft, published }) => ({
+        url: `/${_id}`,
+        method: "PUT",
+        body: { author, content, draft, published },
+      }),
+      invalidatesTags: ["Blog"],
+    }),
+    deleteBlog: builder.mutation<Blog, string>({
+      query: (_id) => ({
+        url: `/${_id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Blog"],
+    }),
+  }),
+})
+
+export const {
+  useGetBlogsQuery,
+  useGetBlogQuery,
+  useDeleteBlogMutation,
+  useUpdateBlogMutation,
+} = blogApi
--- src/app/services/userApi.ts
+++ src/app/services/userApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 import { RootState } from "../store"
-import { User, AuthResponse, Login, Register } from "@/types/user"
+import { AuthResponse, Login, Register } from "@/types/user"
 import { API_URL } from "@/constants"
 
 export const userApi = createApi({
