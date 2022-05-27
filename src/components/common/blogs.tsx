a2a49bbe78249496b89a12ac58953e813355bfde
Franklin Jara <franklinjaradev@gmail.com>
2022-05-26 22:07:32 -0400

    task: adding skeleton for blogs

--- src/components/common/blogs.tsx
+++ src/components/common/blogs.tsx
@@ -10,11 +10,11 @@ const Blogs = () => {
   console.log(data)
 
   return (
-    <div className="m-2 bg-blue-400 ">
+    <div className="grid grid-cols-1 gap-2 m-1">
       {data &&
         data.map((blog) => (
-          <div className="p-1 m-1 bg-yellow-200" key={blog._id}>
-            <h1>{blog.title}</h1>
+          <div className="flex flex-col bg-yellow-200 rounded" key={blog._id}>
+            <h1 className="self-center">{blog.title}</h1>
             <Image src={blog.image} width={400} height={400} alt={blog.title} />
             <h2>By {blog.author}</h2>
             <p>Summary: {blog.summary}</p>
