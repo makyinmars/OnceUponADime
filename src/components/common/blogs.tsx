e9fe8365f5a04b894c99ae7697849559db33e050
Franklin Jara <franklinjaradev@gmail.com>
2022-06-02 22:52:33 -0400

    style: adding desktop style

--- src/components/common/blogs.tsx
+++ src/components/common/blogs.tsx
@@ -6,36 +6,38 @@ import { useGetBlogsQuery } from "@/app/services/blogApi"
 const Blogs = () => {
   const { data } = useGetBlogsQuery()
 
-  console.log(data)
-
   return (
-    <div className="grid grid-cols-1 gap-3 mx-2">
-      {data &&
-        data.map((blog) => (
-          <div
-            className="flex flex-col p-1 bg-gray-100 rounded shadow shadow-xl shadow-violet-800"
-            key={blog._id}
-          >
-            <Image
-              className="rounded"
-              src={blog.image}
-              width={50}
-              height={300}
-              alt={blog.title}
-              priority={true}
-            />
-            <h1 className="self-center pt-1 font-semibold">{blog.title}</h1>
-            <div className="p-1 my-1 rounded bg-violet-200">
-              <h2>By {blog.author}</h2>
-              <p>Summary: {blog.summary}</p>
-              <p>Content: {blog.content}</p>
-              <p>Draft: {blog.draft ? "Yes" : "No"}</p>
-              <p>Published: {blog.published ? "Yes" : "No"}</p>
-              <p>Created: {blog.createdAt}</p>
-              <p>Updated: {blog.updatedAt}</p>
+    <div className="grid items-center justify-center max-w-md grid-cols-1 gap-4 p-2 mx-auto sm:max-w-7xl">
+      <div className="grid items-center justify-center grid-cols-1 gap-3 mx-2 sm:grid-cols-2 lg:grid-cols-3">
+        {data &&
+          data.map((blog) => (
+            <div
+              className="flex flex-col p-1 bg-gray-100 rounded shadow shadow-xl shadow-violet-800"
+              key={blog._id}
+            >
+              <Image
+                className="rounded"
+                src={blog.image}
+                width={300}
+                height={300}
+                alt={blog.title}
+                priority={true}
+              />
+              <h1 className="self-center pt-1 text-lg font-semibold md:text-xl">
+                {blog.title}
+              </h1>
+              <div className="p-1 my-1 rounded bg-violet-200">
+                <h2>By {blog.author}</h2>
+                <p>Summary: {blog.summary}</p>
+                <p>Content: {blog.content}</p>
+                <p>Draft: {blog.draft ? "Yes" : "No"}</p>
+                <p>Published: {blog.published ? "Yes" : "No"}</p>
+                <p>Created: {blog.createdAt}</p>
+                <p>Updated: {blog.updatedAt}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+      </div>
     </div>
   )
 }
