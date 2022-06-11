978bbcc5df0392a2ad7ec762e805dc4a41638779
Franklin Jara <franklinjaradev@gmail.com>
2022-06-10 21:46:56 -0400

    tailwind: fixing tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
