e7ec38df672990ed298705470800e47b2803510a
Franklin Jara <franklinjaradev@gmail.com>
2022-06-27 22:13:34 -0400

    :bug: Generate token to any
    Accepting any to generateToken

--- src/server/utils/generateToken.ts
+++ src/server/utils/generateToken.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (userId: string) => {
+export const generateToken = (userId: any) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET as string, {
     expiresIn: "1h",
   });
