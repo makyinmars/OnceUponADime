fcc86a20ed14634ec13c36a8dcd81ead669866ec
Franklin Jara <franklinjaradev@gmail.com>
2022-07-26 22:17:45 -0400

    :lipstick: Style for blog
    styles for blogs

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -4,24 +4,29 @@ import Link from "next/link"
 import Loading from "@/components/common/loading"
 
 const Blogs = () => {
-  const { data, isError, isLoading } = trpc.useQuery(["blog.getPublishedBlogs"])
+  const { data, isLoading } = trpc.useQuery(["blog.getPublishedBlogs"])
   return (
     <div>
       <h2 className="title">Blogs</h2>
       {isLoading && <Loading />}
-      {data &&
-        data.map((blog) => (
-          <div key={blog.id}>
-            <h3>{blog.title}</h3>
-            <h3>{blog.author}</h3>
-            <p>{blog.summary}</p>
-            <img src={blog.imageUrl} alt={blog.title} />
-            <HtmlParser content={blog.content} />
-            <Link href={`/blogs/${blog.id}`}>
-              <p>View this blog</p>
-            </Link>
-          </div>
-        ))}
+      <div className="grid grid-cols-3 gap-4 bg-transparent">
+        {data &&
+          data.map((blog) => (
+            <div
+              key={blog.id}
+              className="text-center border-2 rounded border-slate-700"
+            >
+              <h3>{blog.title}</h3>
+              <h3>{blog.author}</h3>
+              <p>{blog.summary}</p>
+              <img src={blog.imageUrl} alt={blog.title} className="h-72 w-72" />
+              <HtmlParser content={blog.content} />
+              <Link href={`/blogs/${blog.id}`}>
+                <p>View this blog</p>
+              </Link>
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
